7b3a72464ee6235f4fe0c54920582d394cbdb36f
Khelyus <khamatvalievawork@gmail.com>
2025-09-25 00:05:56 +0500

    first test

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,8 +1,6 @@
-// https://on.cypress.io/api
-
-describe('My First Test', () => {
-  it('visits the app root url', () => {
+describe('Vue App Test', () => {
+  it('should visit the app', () => {
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.contains('You did it!')
   })
 })
