22b7aed19ce9aef533a2a4b73c5dedebd69123a8
Khelyus <khamatvalievawork@gmail.com>
2025-10-17 10:12:44 +0500

    Сделана верстка футера. Также подключена pinia и в App.vue и main.ts  сделаны изменения, чтобы проверить работоспособность

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,33 @@
     <AppBody />
     <AppFooter />
     <main>
-
+      <h1>Чисто для проверки счетчика и что pinia работает</h1>
+      <button @click="increment">Increment</button>
+      <p>Count+: {{ count }}</p>
+      <p>Double count*2: {{doubleCount }}</p>
     </main>
   </div>
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue'
 import AppHeader from "./components/header.vue"
 import AppBody from "./components/body.vue"
 import AppFooter from "./components/footer.vue"
+import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia'
 
-export default {
+export default defineComponent({
   name: "App",
-  components: { AppHeader, AppBody, AppFooter, }
-}
+  components: { AppHeader, AppBody, AppFooter },
+  setup() {
+      const store = useCounterStore()
+      const { count, doubleCount } = storeToRefs(store)
+      const { increment } = store
+
+      return { count, doubleCount, increment }
+
+  }
+})
+
 </script>
